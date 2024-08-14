a3dd51ea37fb569b2e93f718479541b340fe3ba2
kono-meowmeow <aya.ayatsuki@gmail.com>
2024-08-14 23:14:59 +0900

    引数等

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,25 +9,55 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            VStack {
-                Text("Mon")
-                Image(systemName: "sun.max.fill").foregroundColor(Color.red)
-                Text("High: 25")
-                Text("Low: 13")
-            }
-            .padding()
+            DayForecast(day: "Mon", isRainy: false, high: 24, low: 14)
             
-            VStack {
-                Text("Tue")
-                Image(systemName: "cloud.rain.fill").foregroundColor(Color.blue)
-                Text("High: 20")
-                Text("Low: 8")
-            }
-            .padding()
+            DayForecast(day: "Tue", isRainy: true, high: 26, low: 15)
+            
+        DayForecast(day: "Wed", isRainy: false, high: 22, low: 16)
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        }
+        
+        return "sun.max.fill"
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        }
+        
+        return Color.yellow
+    }
+
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(Font.headline)
+            Image(systemName: iconName).foregroundColor(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("High: \(high)")
+                .fontWeight(Font.Weight.semibold)
+            Text("Low: \(low)")
+                .fontWeight(Font.Weight.semibold)
+                .foregroundColor(Color.secondary)
+        }
+        .padding()
+    }
 }
